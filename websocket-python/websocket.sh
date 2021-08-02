b37355e094906f8d568a467173077d982db89cb2
doraemon99-x <75873265+doraemon99-x@users.noreply.github.com>
2021-08-02 22:49:59 +0700

    Update websocket.sh

--- websocket-python/websocket.sh
+++ websocket-python/websocket.sh
@@ -6,7 +6,7 @@ echo Cek Hak Akses...
 sleep 0.5
 cd
 #Buat name user github dan nama folder
-GitUser="fisabiliyusri"
+GitUser="doraemon99-x"
 namafolder="websocket-python"
 #wget https://github.com/${GitUser}/main/${namafolder}/
 
